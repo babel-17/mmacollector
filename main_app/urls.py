1a2aa1d079d47d9fbba8507d92b24c9edb09f644
babel-17 <chicas8414@yahoo.com>
2023-06-04 23:19:44 -0700

    part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
     path('skills/', views.skills_index, name="index"),
+    path('skills/<int:skill_id>/', views.skills_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-skills = [
-  {'name': 'BJJ', 'style': 'grappling', 'description': 'submission', 'years': 5},
-  {'name': 'Boxing', 'style': 'striking', 'description': 'force', 'years': 10},
-]
+from .models import Skill
 
 # Create your views here.
 def home(request):
@@ -13,6 +9,13 @@ def about(request):
     return render(request, 'about.html')
 
 def skills_index(request):
+    skills = Skill.objects.all()
     return render(request, 'skills/index.html', {
         'skills': skills    
+    })
+
+def skills_detail(request, skill_id):
+    skill = Skill.objects.get(id=skill_id)
+    return render(request, 'skills/detail.html', {
+        'skill': skill
     })
