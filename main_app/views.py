c820645525c2df1d4f96ce543ac39e5b26c1616a
babel-17 <chicas8414@yahoo.com>
2023-06-04 21:51:19 -0700

    part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+skills = [
+  {'name': 'BJJ', 'style': 'grappling', 'description': 'submission', 'years': 5},
+  {'name': 'Boxing', 'style': 'striking', 'description': 'force', 'years': 10},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def skills_index(request):
+    return render(request, 'skills/index.html', {
+        'skills': skills    
+    })
